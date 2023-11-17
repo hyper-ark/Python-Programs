8c14776979cec32a7dd8d527174826d1ea76dffd
Arya <72051077+hyper-ark@users.noreply.github.com>
2023-11-17 15:52:04 -0600

    Update boiling_curve.py

--- boiling_curve.py
+++ boiling_curve.py
@@ -1,3 +1,5 @@
+#Write a program named boiling_curve.py that takes as input from the user an excess temperature, and outputs the surface heat flux.
+
 # By submitting this assignment, I agree to the following:
 # "Aggies do not lie, cheat, or steal, or tolerate those who do."
 # "I have not given or received any unauthorized aid on this assignment."
