df3c315adf471b9930b7608b60a6651a5d2e8d47
Arya <72051077+hyper-ark@users.noreply.github.com>
2023-11-17 15:55:30 -0600

    Update double_factorial.py

--- double_factorial.py
+++ double_factorial.py
@@ -1,3 +1,5 @@
+#Write a function named doublefactorial in a file named double_factorial.py that takes in as a parameter one positive integer and returns the double factorial of that integer. Your program does NOT need to include print statements. Please remove ALL input statements for submission.
+
 # By submitting this assignment, I agree to the following:
 # "Aggies do not lie, cheat, or steal, or tolerate those who do."
 # "I have not given or received any unauthorized aid on this assignment."
