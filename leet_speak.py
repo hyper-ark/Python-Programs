aa82ff52c5972397b018d15c0af3c986df63d59a
Arya <72051077+hyper-ark@users.noreply.github.com>
2023-11-17 15:59:10 -0600

    Update leet_speak.py

--- leet_speak.py
+++ leet_speak.py
@@ -1,3 +1,5 @@
+#Write a program named leet_speak.py that takes as input from the user a string of text, converts the words to leet, and prints the converted text. Your program must use a dictionary.
+
 # By submitting this assignment, I agree to the following:
 # "Aggies do not lie, cheat, or steal, or tolerate those who do."
 # "I have not given or received any unauthorized aid on this assignment."
