896a4173da38a03d5d51f100248365496aa53337
Arya <72051077+hyper-ark@users.noreply.github.com>
2023-11-17 15:31:20 -0600

    Update using_input.py

--- using_input.py
+++ using_input.py
@@ -1,3 +1,5 @@
+#Convert your using_variables.py program to a new program named using_input.py that produces identical output. However, your new program should take in input from the user as appropriate, store values in variables, and output in the required format.
+
 # By submitting this assignment, I agree to the following:
 # "Aggies do not lie, cheat, or steal, or tolerate those who do."
 # "I have not given or received any unauthorized aid on this assignment."
