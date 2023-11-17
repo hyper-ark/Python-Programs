f7af1e7a85fd3d8f9cb5327c0d6cc01c3df43d4f
Arya <72051077+hyper-ark@users.noreply.github.com>
2023-11-17 15:48:52 -0600

    Update roundoff_error.py

--- roundoff_error.py
+++ roundoff_error.py
@@ -1,3 +1,5 @@
+#Write a program named roundoff_error.py according to the instructions given.
+
 # By submitting this assignment, I agree to the following:
 # "Aggies do not lie, cheat, or steal, or tolerate those who do."
 # "I have not given or received any unauthorized aid on this assignment."
