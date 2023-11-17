868f5977684a90f6bbf8dc72e41df1811fe38656
Arya <72051077+hyper-ark@users.noreply.github.com>
2023-11-17 15:39:18 -0600

    Update writing_functions.py

--- writing_functions.py
+++ writing_functions.py
@@ -1,3 +1,5 @@
+#Write a program named writing_functions.py that calculates the area of several regular polygons when given a side length.
+
 # By submitting this assignment, I agree to the following:
 # "Aggies do not lie, cheat, or steal, or tolerate those who do."
 # "I have not given or received any unauthorized aid on this assignment."
