1995312f6e03b11b5bd81417550d4d951ab95090
Arya <72051077+hyper-ark@users.noreply.github.com>
2023-11-17 15:57:03 -0600

    Update balancing_numbers.py

--- balancing_numbers.py
+++ balancing_numbers.py
@@ -1,3 +1,5 @@
+#Write a program named balancing_numbers.py that takes in an integer value n from the user and determines if it is a balancing number. If n is a balancing number, output the corresponding value of r. Do NOT use any containers like lists, tuples, sets, and dictionaries, or the sum() function.
+
 # By submitting this assignment, I agree to the following:
 # "Aggies do not lie, cheat, or steal, or tolerate those who do."
 # "I have not given or received any unauthorized aid on this assignment."
